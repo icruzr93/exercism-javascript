2019e58b73861db7088549bbf267bc21b113c0de
Isaac <isaac@ktbo.mx>
2019-02-17 23:47:55 -0600

    Bubble Right

--- sort-bubble/sort-bubble.js
+++ sort-bubble/sort-bubble.js
@@ -1,11 +1,11 @@
 
 export const sortBubble = (arr) => {
-  for(let i1 = 0; i1 < arr.length; i1++){
-    for(let i2 = 0; i2 < arr.length; i2++){
-      if(arr[i2] > arr[i1]){
-        let temp = arr[i2];
-        arr[i2] = arr[i1];
-        arr[i1] = temp;
+  for (let index1 = 0; index1 < arr.length; index1 += 1) {
+    for (let index2 = 0; index2 < arr.length - 1 - index1; index2 += 1) {
+      if (arr[index2] > arr[index2 + 1]) {
+        const temp = arr[index2];
+        arr[index2] = arr[index2 + 1];
+        arr[index2 + 1] = temp;
       }
     }
   }
